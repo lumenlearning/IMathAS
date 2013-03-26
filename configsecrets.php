d0a827f2d46d2d8966ae50aede69df04270fd6ef
Bowen Masco <bowen@codingfoo.com>
2013-03-26 14:37:47 -0600

    setup config to fetch secrets from the environment

---
+++ configsecrets.php
@@ -0,0 +1,10 @@
+<?php
+//MOM account secrets
+$AWSkey = $_ENV['AWS_ACCESS_KEY_ID'];
+$AWSsecret = $_ENV['AWS_SECRET_KEY'];
+$AWSbucket = $_ENV['PARAM1'];
+$dbserver = $_ENV['PARAM2'];
+$dbname = $_ENV['PARAM3'];
+$dbusername = $_ENV['PARAM4'];
+$dbpassword = $_ENV['PARAM5'];
+?>
