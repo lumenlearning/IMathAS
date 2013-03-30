f9baef8326cfa5910dc4c06ea8f78f4ec35a93b3
drlippman <drlippman@gmail.com>
2013-03-29 18:50:05 -0700

    Move enviro pulls from configsecrets into config.php

--- config.php
+++ config.php
@@ -6,8 +6,16 @@
 $imasroot = "";
 
 //database access settings
-$curdir = rtrim(dirname(__FILE__), '/\\');
-require("$curdir/configsecrets.php");
+$AWSkey = getenv('AWS_ACCESS_KEY_ID');
+$AWSsecret = getenv('AWS_SECRET_KEY');
+$AWSbucket = getenv('PARAM1');
+$dbserver = getenv('PARAM2');
+$dbname = getenv('PARAM3');
+$dbusername = getenv('PARAM4');
+$dbpassword = getenv('PARAM5');
+if (getenv('imasroot')!==false) {
+	$imasroot = getenv('imasroot');
+}
 $CFG['GEN']['AWSforcoursefiles'] = true;
 
 //install name
