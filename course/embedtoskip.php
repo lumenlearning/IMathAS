94e5af0e77143fa39d557c15062e6fe76c45c2d5
drlippman <drlippman@gmail.com>
2014-03-26 18:54:55 -0700

    Add embed to skip script

---
+++ course/embedtoskip.php
@@ -0,0 +1,49 @@
+<?php
+require("../validate.php");
+
+if (!isset($teacherid)) {
+	echo "Must be a teacher to access this page";
+	exit;
+}
+
+$id = intval($_GET['id']);
+
+$query = "SELECT intro FROM imas_assessments WHERE id=$id";
+$result = mysql_query($query) or die("Query failed :$query " . mysql_error());
+$row = mysql_fetch_row($result);
+
+$text = preg_replace('/<p[^>]*>(\s|&nbsp;)*(\[QUESTION.*?\])(\s|&nbsp;)*<\/p>/sm', ' $2 ', $row[0]);
+$text = preg_replace('/<p[^>]*>(\s|&nbsp;)*<span[^>]*>(\s|&nbsp;)*(\[QUESTION.*?\])(\s|&nbsp;)*<\/span>(\s|&nbsp;)*<\/p>/sm', ' $3 ', $text);
+
+$sp = preg_split('/\[QUESTION\s*(\d+)\]/', $text, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+$n = 0;
+$out = '';
+
+while (isset($sp[$n])) {
+	$text = trim($sp[$n]);
+	if (isset($sp[$n+1])) {
+		$qn = array($sp[$n+1]);
+		$n+=2;
+		while (isset($sp[$n]) && trim($sp[$n])=='' && isset($sp[$n+1])) {
+			$qn[1] = $sp[$n+1];
+			$n+=2;
+		}
+		$out .= '<p>';
+		if (count($qn)==1) {
+			$out .= '[Q '.$qn[0].']';
+		} else {
+			$out .= '[Q '.implode('-',$qn).']';
+		}
+		$out .= '</p>';
+	} else {
+		$n+=2;
+	}
+	$out .= $text;
+}
+
+$out = addslashes($out);
+$query = "UPDATE imas_assessments SET intro='$out',displaymethod='SkipAround' WHERE id='$id'";
+mysql_query($query) or die("Query failed :$query " . mysql_error());
+
+?>
