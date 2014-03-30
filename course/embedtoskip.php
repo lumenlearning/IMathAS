3ec16a74734bfcda0c7f4967954225ed801e1dc5
drlippman <drlippman@gmail.com>
2014-03-30 10:05:00 -0700

    strip [PAGE] tags in embedtoskip

--- course/embedtoskip.php
+++ course/embedtoskip.php
@@ -12,8 +12,11 @@ $query = "SELECT intro FROM imas_assessments WHERE id=$id";
 $result = mysql_query($query) or die("Query failed :$query " . mysql_error());
 $row = mysql_fetch_row($result);
 
+$text = preg_replace('/[PAGE[^\]]*]/sm','',$text);
 $text = preg_replace('/<p[^>]*>(\s|&nbsp;)*(\[QUESTION.*?\])(\s|&nbsp;)*<\/p>/sm', ' $2 ', $row[0]);
+$text = preg_replace('/<p[^>]*>(\s|&nbsp;)*(\[PAGE.*?\])(\s|&nbsp;)*<\/p>/sm', '', $text);
 $text = preg_replace('/<p[^>]*>(\s|&nbsp;)*<span[^>]*>(\s|&nbsp;)*(\[QUESTION.*?\])(\s|&nbsp;)*<\/span>(\s|&nbsp;)*<\/p>/sm', ' $3 ', $text);
+$text = preg_replace('/<p[^>]*>(\s|&nbsp;)*<span[^>]*>(\s|&nbsp;)*(\[PAGE.*?\])(\s|&nbsp;)*<\/span>(\s|&nbsp;)*<\/p>/sm', '', $text);
 
 $sp = preg_split('/\[QUESTION\s*(\d+)\]/', $text, -1, PREG_SPLIT_DELIM_CAPTURE);
 
@@ -22,23 +25,24 @@ $out = '';
 
 while (isset($sp[$n])) {
 	$text = trim($sp[$n]);
-	if (isset($sp[$n+1])) {
-		$qn = array($sp[$n+1]);
-		$n+=2;
-		while (isset($sp[$n]) && trim($sp[$n])=='' && isset($sp[$n+1])) {
-			$qn[1] = $sp[$n+1];
-			$n+=2;
-		}
-		$out .= '<p>';
-		if (count($qn)==1) {
-			$out .= '[Q '.$qn[0].']';
-		} else {
-			$out .= '[Q '.implode('-',$qn).']';
-		}
-		$out .= '</p>';
-	} else {
+	$qn = array($sp[$n+1]);
+	$n+=2;
+	while (isset($sp[$n]) && trim($sp[$n])=='' && isset($sp[$n+1])) {
+		$qn[1] = $sp[$n+1];
 		$n+=2;
 	}
+	if (isset($sp[$n]) && !isset($sp[$n+1])) { //last item in the set
+		$text .= trim($sp[$n]);
+		$n++;
+	}
+	$out .= '<p>';
+	if (count($qn)==1) {
+		$out .= '[Q '.$qn[0].']';
+	} else {
+		$out .= '[Q '.implode('-',$qn).']';
+	}
+	$out .= '</p>';
+	
 	$out .= $text;
 }
 
