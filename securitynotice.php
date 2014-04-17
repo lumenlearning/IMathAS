814ffbc1f73fceb44d8098944f9ac415e619b126
drlippman <drlippman@gmail.com>
2014-04-17 15:31:42 -0700

    Add securitynotice

---
+++ securitynotice.php
@@ -0,0 +1,16 @@
+<?php
+require("config.php");
+require("header.php");
+?>
+<p dir="ltr"><strong>Subject: Action Required: Reset MyOpenMath Password</strong></p>
+<p dir="ltr"><strong>From: &nbsp;David Lippman, Lumen Learning MyOpenMath Lead</strong></p>
+<p dir="ltr"><span>Due to a potential security breach that occurred earlier today, we are requiring all MyOpenMath and WAMAP users to reset their passwords. On your next visit to MyOpenMath or WAMAP, you will be directed to reset your password by clicking on the "Forgot Password" link.</span></p>
+<p dir="ltr"><span>Further information about the security incident follows.</span></p>
+<p dir="ltr"><span>This morning at approximately 10:00 AM PT, an unauthorized user attempted to gain access to usernames and encrypted passwords on MyOpenMath.com and WAMAP.org. We discovered the intrusion attempt as it was in process and stopped the attack. We have no positive evidence that the attacker gained access to any user information, but we will continue to investigate that possibility and inform all users of the results of that investigation.</span></p>
+<p dir="ltr"><span>We can confirm the user did not attempt to access student grade data, and we can also confirm no actual passwords were revealed. If encrypted passwords were accessed, an attacker with sufficient technical expertise could have the ability to guess or unencrypt passwords, particularly a weak password. At this time we have no evidence to suggest this has taken place.</span></p>
+<p dir="ltr"><span>The mandatory password reset is a security precaution. We have addressed the vulnerability that was exposed and will continue to investigate the incident and take steps to prevent future security issues.</span></p>
+<p dir="ltr"><span>When our investigation is complete, we will inform you about the results and any impact on MyOpenMath users. If you have questions or would like a more complete summary of the detection and investigation of this incident, please email </span><a href="mailto:support@lumenlearning.com"><span>support@lumenlearning.com</span></a><span>. In the unlikely event that you notice suspicious activity on your MyOpenMath or WAMAP account, please notify us immediately.</span></p>
+<p><span><span>We apologize for the inconvenience this has caused and thank you for your patience as we work through this issue. We remain committed to providing a safe and secure experience for MyOpenMath and WAMAP users.</span></span></p>
+<?php
+require("footer.php");
+?>
