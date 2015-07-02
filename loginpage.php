18d5dae7fc303d0ced873dbb7b743863f1c037a0
drlippman <drlippman@gmail.com>
2015-07-01 19:41:40 -0700

    Add Katex experimental to the loginpages Add favicon for MOM

--- loginpage.php
+++ loginpage.php
@@ -84,6 +84,7 @@ if (!isset($imasroot)) { //don't allow direct access to loginpage.php
 		html += "<a href='#' onClick=\"window.open('<?php echo $imasroot;?>/help.php?section=loggingin','help','top=0,width=400,height=500,scrollbars=1,left='+(screen.width-420))\">Help</a>";
 		html += '<div style="margin-top: 0px;margin-right:0px;text-align:right;padding:0px"><select name="access"><option value="0">Use defaults</option>';
 		html += '<option value="3">Force image-based display</option>';
+		html += '<option value="6">Use KaTeX display (experimental)</option>';
 		html += '<option value="1">Use text-based display</option></select></div>';
 		
 		if (!MathJaxCompatible) {
