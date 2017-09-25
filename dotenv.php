ffb2a81b70f95a20fef966df403ac2410ed84e38
Nessa Marin <nessa.murmur@gmail.com>
2017-09-25 10:55:35 -0700

    Set envvars through `putenv`

--- dotenv.php
+++ dotenv.php
@@ -3,10 +3,7 @@
 $envvars = file('.env');
 
 foreach ($envvars as $num => $envvar) {
-    $key_and_value = explode("=", $envvar);
-    $key = $key_and_value[0];
-    $value = $key_and_value[1];
-    $ENV[$key] = $value;
+    putenv($value);
 }
 
 ?>
