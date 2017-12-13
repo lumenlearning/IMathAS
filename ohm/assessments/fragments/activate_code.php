a1b8badaffdf7f0caf98753dccc6950263f42734
Michael Ackerman <michael@lumenlearning.com>
2017-12-13 12:09:27 -0800

    Fixed: We're passing "0" to Lumenistration when activating a code.

--- ohm/assessments/fragments/activate_code.php
+++ ohm/assessments/fragments/activate_code.php
@@ -5,7 +5,7 @@
 ?>
 <form method="POST" action="<?php echo $GLOBALS['basesiteurl']; ?>/ohm/assessments/process_activation.php">
     <input type="hidden" name="action" value="activate_code"/>
-    <input type="hidden" name="group_id" value="<?php echo $GLOBALS['groupid']; ?>"/>
+    <input type="hidden" name="group_id" value="<?php echo $courseOwnerGroupId; ?>"/>
     <input type="hidden" name="course_id" value="<?php echo Sanitize::courseId($courseId); ?>"/>
     <input type="hidden" name="assessment_id" value="<?php echo Sanitize::onlyInt($assessmentId); ?>"/>
     <div class="access_code_input_wrapper">
