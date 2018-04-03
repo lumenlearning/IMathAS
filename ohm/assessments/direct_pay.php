1d71b4a5284ac0c2464e71e85daa24783a0532a6
Michael Ackerman <michael@lumenlearning.com>
2018-04-03 12:15:30 -0700

    Use cdnjs.com for getting React.

--- ohm/assessments/direct_pay.php
+++ ohm/assessments/direct_pay.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . "/../../header.php");
 
 <div id="directPay"></div>
 
-<script src="https://fb.me/react-0.13.3.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react.min.js"></script>
 <script src="../../../lumen-components/build/vanilla/render_direct_pay_component.js"></script>
 <script>
   renderDirectPayComponent.renderDirectPayComponent('directPay');
