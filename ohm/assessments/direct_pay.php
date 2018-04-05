a643ad49fb939f471266cdb5701ffd28c720fe6b
Michael Ackerman <michael@lumenlearning.com>
2018-04-05 16:58:48 -0700

    Pass required data to the direct pay component.
    Note: This is a non-functional commit.

--- ohm/assessments/direct_pay.php
+++ ohm/assessments/direct_pay.php
@@ -1,16 +1,25 @@
 <?php
 require_once(__DIR__ . "/../../init.php");
 require_once(__DIR__ . "/../../header.php");
+
+$endpoint = $GLOBALS["basesiteurl"]
+    . '/ohm/assessments/direct_pay.php?action=payment_proxy';
+$apiKey = $GLOBALS["student_pay_api"]["stripe_api_key"];
+// FIXME: How are we pricing things? Where is this value coming from??
+$amount = '3000'; // must be a string
+$redirectTo = $GLOBALS["basesiteurl"] . '/ohm/assessment/showtest.php';
 ?>
 
 <div id="directPay"></div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.13.3/react.min.js"></script>
-<script src="../../../lumen-components/build/vanilla/render_direct_pay_component.js"></script>
+<script src="../../../lumen-components/build/vanilla_js/direct_pay_components.js"></script>
 <script>
-  renderDirectPayComponent.renderDirectPayComponent('directPay', {
-    'endpoint': '<?php echo $GLOBALS['basesiteurl']
-        . "/ohm/assessments/direct_pay.php?action=payment_proxy" ?>'
+  directPayComponents.renderDirectPayLandingPage('directPay', {
+    'endpoint': '<?php echo $endpoint; ?>',
+    'stripeKey': '<?php echo $apiKey; ?>',
+    'amount': '<?php echo $amount; ?>',
+    'redirectTo': '<?php echo $redirectTo; ?>',
   });
 </script>
 
