cc85f3e3ed81bc1882a6ed56380e50f876b5c03b
Michael Ackerman <michael@lumenlearning.com>
2018-04-16 17:37:17 -0700

    Setup DB interaction for SlimPHP API.

--- ohm/api/src/configs/settings-example.php
+++ ohm/api/src/configs/settings-example.php
@@ -1,8 +1,14 @@
 <?php
 return [
 	'settings' => [
-		'displayErrorDetails' => true, // set to false in production
+		'displayErrorDetails' => false, // set to false in production
 		'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+		// API general settings
+		'api' => [
+			'defaultPageSize' => 10,
+			'maxPageSize' => 100,
+		],
 
 		// Renderer settings
 		'renderer' => [
@@ -12,8 +18,22 @@ return [
 		// Monolog settings
 		'logger' => [
 			'name' => 'slim-app',
-			'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../../logs/app.log',
+//			'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../../logs/app.log',
+			'path' => 'php://stdout', // FIXME: Configure real logging for Beanstalk
 			'level' => \Monolog\Logger::DEBUG,
 		],
+
+		// Database settings
+		'determineRouteBeforeAppMiddleware' => false,
+		'db' => [
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'database' => 'database',
+			'username' => 'username',
+			'password' => 'password',
+			'charset' => 'latin1',
+			'collation' => 'latin1_swedish_ci',
+			'prefix' => '',
+		]
 	],
 ];
