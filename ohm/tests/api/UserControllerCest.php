8e32721ee74938361ae59528834b07f0b0a00a2a
Michael Ackerman <michael@lumenlearning.com>
2018-04-17 13:24:51 -0700

    Redo local hacks to make tests work.

--- ohm/tests/api/UserControllerCest.php
+++ ohm/tests/api/UserControllerCest.php
@@ -29,32 +29,32 @@ class UserControllerCest extends BaseCestCase
 	public function findAll($I)
 	{
 		$I->sendGet('/v1/users');
-		$I->seeResponseCodeIs(200);
+		$I->seeResponseCodeIs(HttpCode::OK);
 		$I->seeResponseJsonMatchesJsonPath('$[*].SID');
 		$I->seeResponseMatchesJsonType([
 			'id' => 'integer',
 			'SID' => 'string',
-			'rights' => 'integer', // integer (as string on PHP 5)
+			'rights' => 'integer|string', // integer (as string on PHP 5)
 			'FirstName' => 'string',
 			'LastName' => 'string',
 			'email' => 'string',
-			'lastaccess' => 'integer', // integer (as string on PHP 5)
-			'groupid' => 'integer', // integer (as string on PHP 5)
-			'msgnotify' => 'integer', // tinyint(1) (as string on PHP 5)
-			'qrightsdef' => 'integer', // tinyint(1) (as string on PHP 5)
-			'deflib' => 'integer', // integer (as string on PHP 5)
-			'usedeflib' => 'integer', // tinyint(1) (as string on PHP 5)
-			'homelayout' => 'string',
-			'hasuserimg' => 'integer', // tinyint(1) (as string on PHP 5)
-			'remoteaccess' => 'string',
+			'lastaccess' => 'integer|string', // integer (as string on PHP 5)
+			'groupid' => 'integer|string', // integer (as string on PHP 5)
+			'msgnotify' => 'integer|string', // tinyint(1) (as string on PHP 5)
+			'qrightsdef' => 'integer|string', // tinyint(1) (as string on PHP 5)
+			'deflib' => 'integer|string', // integer (as string on PHP 5)
+			'usedeflib' => 'integer|string', // tinyint(1) (as string on PHP 5)
+			'homelayout' => 'string|string',
+			'hasuserimg' => 'integer|string', // tinyint(1) (as string on PHP 5)
+			'remoteaccess' => 'string|string',
 			'theme' => 'string',
-			'listperpage' => 'integer', // integer (as string on PHP 5)
+			'listperpage' => 'integer|string', // integer (as string on PHP 5)
 			'hideonpostswidget' => 'string',
-			'specialrights' => 'integer', // integer (as string on PHP 5)
+			'specialrights' => 'integer|string', // integer (as string on PHP 5)
 			'FCMtoken' => 'string',
 			'jsondata' => 'string',
-			'created_at' => 'integer|null', // integer (as string on PHP 5)
-			'forcepwreset' => 'integer', // integer (as string on PHP 5)
+			'created_at' => 'integer|null|string', // integer (as string on PHP 5)
+			'forcepwreset' => 'integer|string', // integer (as string on PHP 5)
 		], '$[*]');
 	}
 }
