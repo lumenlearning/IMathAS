67cc1c2f808c03548b9b25cc24dffcf636ae5c63
Michael Ackerman <michael@lumenlearning.com>
2018-04-17 17:43:21 -0700

    Attempts to make Travis work with login/logout during tests.

--- ohm/tests/api/BaseCestCase.php
+++ ohm/tests/api/BaseCestCase.php
@@ -3,6 +3,7 @@
 namespace OHM\Tests\Api;
 
 use ApiTester;
+use Codeception\Util\HttpCode;
 
 class BaseCestCase
 {
@@ -15,9 +16,11 @@ class BaseCestCase
 	protected function userLogin($I)
 	{
 		$I->amOnPage('/../../../index.php');
+		$I->see('Request an instructor account');
 		$I->fillField('username', 'root');
 		$I->fillField('password', 'root');
 		$I->click('Login');
+		$I->seeResponseCodeIs(HttpCode::OK);
 		$I->see('Welcome to Lumen OHM');
 
 		$this->loggedIn = true;
@@ -34,6 +37,7 @@ class BaseCestCase
 
 		$I->amOnPage('/../../../index.php');
 		$I->click(['link' => 'Log Out']);
+		$I->seeResponseCodeIs(HttpCode::OK);
 		$I->see('Request an instructor account');
 
 		$this->loggedIn = false;
