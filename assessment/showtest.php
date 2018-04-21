73e7fd886c979ccd3be8faf446e2775bef987c9c
Michael Ackerman <michael@lumenlearning.com>
2018-04-20 18:08:30 -0700

    Moved paywall code out of showtest.php so we can use it in multiple places.
    At this point, all paywall related files could now be redesigned in an OOP
    way, with some minor hackery in showtest.php to make it all work. This
    should be revisited, someday.

--- assessment/showtest.php
+++ assessment/showtest.php
@@ -217,6 +217,22 @@
 			
 		}
 
+
+		// #### Begin OHM-specific code #####################################################
+		// #### Begin OHM-specific code #####################################################
+		// #### Begin OHM-specific code #####################################################
+		// #### Begin OHM-specific code #####################################################
+		// #### Begin OHM-specific code #####################################################
+
+		require(__DIR__ . '/../ohm/assessments/paywall_start.php');
+
+		// #### End OHM-specific code #######################################################
+		// #### End OHM-specific code #######################################################
+		// #### End OHM-specific code #######################################################
+		// #### End OHM-specific code #######################################################
+		// #### End OHM-specific code #######################################################
+
+
 		//check for password
 
 		if (trim($adata['password'])!='' && preg_match('/^\d{1,3}\.(\*|\d{1,3})\.(\*|\d{1,3})\.[\d\*\-]+/',$adata['password'])) {
@@ -598,7 +614,7 @@
 			// #### End OHM-specific code #######################################################
 			// #### End OHM-specific code #######################################################
 			// #### End OHM-specific code #######################################################
-			// #### End OHM-specific code #######################################################
+			// #### En1Yd OHM-specific code #######################################################
 			// #### End OHM-specific code #######################################################
 
 			// OHM-specific code:
@@ -627,96 +643,20 @@
 	$stm->execute(array(':id'=>$testid));
 	$line = $stm->fetch(PDO::FETCH_ASSOC);
 
-
 	// #### Begin OHM-specific code #####################################################
 	// #### Begin OHM-specific code #####################################################
 	// #### Begin OHM-specific code #####################################################
 	// #### Begin OHM-specific code #####################################################
 	// #### Begin OHM-specific code #####################################################
 
-	/*
-	 * So I won't forget for a 6th time and have to delete an hour of nice refactoring:
-	 *
-	 * This is all in global scope because we are using core MOM's validate.php, header.php,
-	 * and footer.php files. These all depend on many things declared in global scope. Don't
-	 * remove this OHM-specific code block from global scope unless you're prepared to deal
-	 * with that.
-	 */
+	$assessmentIdFromDb = $line['assessmentid'];
+	require(__DIR__ . '/../ohm/assessments/paywall_start.php');
 
-	// The business decision is to allow students through to assessments if we encounter any
-	// problems checking access codes. This includes failure to interact with the payment API.
-
-	$courseId = $sessiondata['courseid'];
-	$courseName = $sessiondata['coursename'];
-	$assessmentId = $line['assessmentid'];
-
-	$courseOwnerGroupId = null;
-	if (isStudentPayEnabled()) {
-		require_once(__DIR__ . "/../ohm/includes/StudentPaymentDb.php");
-		$studentPaymentDb = new \OHM\StudentPaymentDb(null, $courseId, null);
-		$courseOwnerGroupId = $studentPaymentDb->getCourseOwnerGroupId();
-
-		// We need the course owner's group ID before we can check a student's access code status.
-		if (!isValidGroupIdForStudentPayments($courseOwnerGroupId)) {
-			error_log(sprintf(
-				"Course owner is not a member of a group for course ID %d. Unable to get student access code status.",
-				$courseId));
-		}
-	}
-
-	$studentPayment = null;
-	$studentPayStatus = null;
-	if (isStudentPayEnabled() && isValidGroupIdForStudentPayments($courseOwnerGroupId)) {
-		require_once(__DIR__ . "/../ohm/includes/StudentPayment.php");
-		require_once(__DIR__ . "/../ohm/models/StudentPayStatus.php");
-		$studentPayment = new \OHM\StudentPayment($courseOwnerGroupId, $GLOBALS['cid'], $GLOBALS['userid']);
-
-		try {
-			$studentPayStatus = $studentPayment->getCourseAndStudentPaymentInfo();
-		} catch (OHM\StudentPaymentException $e) {
-			// See notes above re: business decisions
-			error_log("Student payment API error: " . $e->getMessage());
-			error_log("Stack trace: " . $e->getTraceAsString());
-		}
-
-		if (!is_null($studentPayStatus)) {
-			$courseRequiresPayment = $studentPayStatus->getCourseRequiresStudentPayment();
-			$studentHasAccessCode = $studentPayStatus->getStudentHasValidAccessCode();
-			$paymentTypeRequired = $studentPayStatus->getStudentPaymentTypeRequired();
-
-			if (\OHM\StudentPayApiResult::ACCESS_TYPE_ACTIVATION_CODE ==
-				$paymentTypeRequired && $courseRequiresPayment) {
-				if (!$studentHasAccessCode) {
-					require_once(__DIR__ . "/../ohm/assessments/activation.php");
-				}
-			}
-
-			if (\OHM\StudentPayApiResult::ACCESS_TYPE_DIRECT_PAY ==
-				$paymentTypeRequired && $courseRequiresPayment) {
-				if (!$studentHasAccessCode) {
-					require_once(__DIR__ . "/../ohm/assessments/direct_pay.php");
-				}
-			}
-
-		}
-	}
-
-	if (!is_null($studentPayStatus) && $studentPayStatus->getStudentIsInTrial()) {
-		$shouldLogEvent = array('begin_trial', 'extend_trial', 'continue_trial');
-		$logEventType = getActivationLogEventType();
-
-		if (in_array($logEventType, $shouldLogEvent)) {
-			$studentPayment->logTakeAssessmentDuringTrial($assessmentId);
-			unsetActivationLogEventType();
-		}
-	}
-
-	// #### End OHM-specific code #######################################################
-	// #### End OHM-specific code #######################################################
-	// #### End OHM-specific code #######################################################
-	// #### End OHM-specific code #######################################################
-	// #### End OHM-specific code #######################################################
-
+	// #### Begin OHM-specific code #####################################################
+	// #### Begin OHM-specific code #####################################################
+	// #### Begin OHM-specific code #####################################################
+	// #### Begin OHM-specific code #####################################################
+	// #### Begin OHM-specific code #####################################################
 
 	$GLOBALS['assessver'] = $line['ver'];
 	if (strpos($line['questions'],';')===false) {
@@ -3918,59 +3858,6 @@ if (!isset($_REQUEST['embedpostback']) && empty($_POST['backgroundsaveforlater']
 	// #### Begin OHM-specific code #####################################################
 	// #### Begin OHM-specific code #####################################################
 	// #### Begin OHM-specific code #####################################################
-	/**
-	 * Determine if student payment status should be checked for assessments.
-	 *
-	 * @return boolean True if yes, False if no.
-	 */
-	function isStudentPayEnabled()
-	{
-		// 20 = student
-		if (20 > $GLOBALS['myrights'] && isset($GLOBALS['student_pay_api']) && $GLOBALS['student_pay_api']['enabled']) {
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
-	 * Determine if a group ID is valid for student payments.
-	 *
-	 * @param $groupId integer The group ID.
-	 * @return bool True if yes, False if no.
-	 */
-	function isValidGroupIdForStudentPayments($groupId) {
-		if (is_null($groupId) || 0 >= $groupId) {
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Return the student payments type of event we want to log.
-	 *
-	 * This information is obtained from a session cookie or a query string value.
-	 *
-	 * @return null
-	 */
-	function getActivationLogEventType()
-	{
-		if (isset($_COOKIE['activation_event'])) {
-			return $_COOKIE['activation_event'];
-		} else if (!is_null($_REQUEST['activation_event'])) {
-			return $_REQUEST['activation_event'];
-		} else {
-			return null;
-		}
-	}
-
-	function unsetActivationLogEventType()
-	{
-		setcookie("activation_event", "", -1, '/');
-		unset($_COOKIE['activation_event']);
-	}
-
 	/**
 	 * Determine if a user is starting an assessment.
 	 *
---
+++ ohm/assessments/paywall_start.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Enforce all applicable Lumen OHM student payment rules.
+ * (activation codes, direct payments, trials, etc)
+ *
+ * This function should be used in places where an assessment is about
+ * to be displayed to the student.
+ */
+
+/*
+ * So I won't forget for a 6th time and have to delete an hour of nice refactoring:
+ *
+ * This is all in global scope because we are using core MOM's validate.php, header.php,
+ * and footer.php files. These all depend on many things declared in global scope. Don't
+ * remove this OHM-specific code block from global scope unless you're prepared to deal
+ * with that.
+ */
+
+// The business decision is to allow students through to assessments if we encounter any
+// problems checking access codes. This includes failure to interact with the payment API.
+
+$courseId = $sessiondata['courseid'];
+$courseName = $sessiondata['coursename'];
+$assessmentId = isset($_GET['id']) ? intval($_GET['id']) : $assessmentIdFromDb;
+
+$courseOwnerGroupId = null;
+if (isStudentPayEnabled()) {
+	require_once(__DIR__ . "/../../ohm/includes/StudentPaymentDb.php");
+	$studentPaymentDb = new \OHM\StudentPaymentDb(null, $courseId, null);
+	$courseOwnerGroupId = $studentPaymentDb->getCourseOwnerGroupId();
+
+	// We need the course owner's group ID before we can check a student's access code status.
+	if (!isValidGroupIdForStudentPayments($courseOwnerGroupId)) {
+		error_log(sprintf(
+			"Course owner is not a member of a group for course ID %d. Unable to get student access code status.",
+			$courseId));
+	}
+}
+
+$studentPayment = null;
+$studentPayStatus = null;
+if (isStudentPayEnabled() && isValidGroupIdForStudentPayments($courseOwnerGroupId)) {
+	require_once(__DIR__ . "/../../ohm/includes/StudentPayment.php");
+	require_once(__DIR__ . "/../../ohm/models/StudentPayStatus.php");
+	$studentPayment = new \OHM\StudentPayment($courseOwnerGroupId, $GLOBALS['cid'], $GLOBALS['userid']);
+
+	try {
+		$studentPayStatus = $studentPayment->getCourseAndStudentPaymentInfo();
+	} catch (OHM\StudentPaymentException $e) {
+		// See notes above re: business decisions
+		error_log("Student payment API error: " . $e->getMessage());
+		error_log("Stack trace: " . $e->getTraceAsString());
+	}
+
+	if (!is_null($studentPayStatus)) {
+		$courseRequiresPayment = $studentPayStatus->getCourseRequiresStudentPayment();
+		$studentHasAccessCode = $studentPayStatus->getStudentHasValidAccessCode();
+		$paymentTypeRequired = $studentPayStatus->getStudentPaymentTypeRequired();
+
+		if (\OHM\StudentPayApiResult::ACCESS_TYPE_ACTIVATION_CODE ==
+			$paymentTypeRequired && $courseRequiresPayment) {
+			if (!$studentHasAccessCode) {
+				require_once(__DIR__ . "/../../ohm/assessments/activation.php");
+			}
+		}
+
+		if (\OHM\StudentPayApiResult::ACCESS_TYPE_DIRECT_PAY ==
+			$paymentTypeRequired && $courseRequiresPayment) {
+			if (!$studentHasAccessCode) {
+				require_once(__DIR__ . "/../../ohm/assessments/direct_pay.php");
+			}
+		}
+
+	}
+}
+
+if (!is_null($studentPayStatus) && $studentPayStatus->getStudentIsInTrial()) {
+	$shouldLogEvent = array('begin_trial', 'extend_trial', 'continue_trial');
+	$logEventType = getActivationLogEventType();
+
+	if (in_array($logEventType, $shouldLogEvent)) {
+		$studentPayment->logTakeAssessmentDuringTrial($assessmentId);
+		unsetActivationLogEventType();
+	}
+}
+
+
+/**
+ * Determine if student payment status should be checked for assessments.
+ *
+ * @return boolean True if yes, False if no.
+ */
+function isStudentPayEnabled()
+{
+	// 20 = student
+	if (20 > $GLOBALS['myrights'] && isset($GLOBALS['student_pay_api']) && $GLOBALS['student_pay_api']['enabled']) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Determine if a group ID is valid for student payments.
+ *
+ * @param $groupId integer The group ID.
+ * @return bool True if yes, False if no.
+ */
+function isValidGroupIdForStudentPayments($groupId)
+{
+	if (is_null($groupId) || 0 >= $groupId) {
+		return false;
+	}
+
+	return true;
+}
+
+/**
+ * Return the student payments type of event we want to log.
+ *
+ * This information is obtained from a session cookie or a query string value.
+ *
+ * @return null
+ */
+function getActivationLogEventType()
+{
+	if (isset($_COOKIE['activation_event'])) {
+		return $_COOKIE['activation_event'];
+	} else if (!is_null($_REQUEST['activation_event'])) {
+		return $_REQUEST['activation_event'];
+	} else {
+		return null;
+	}
+}
+
+function unsetActivationLogEventType()
+{
+	setcookie("activation_event", "", -1, '/');
+	unset($_COOKIE['activation_event']);
+}
+
