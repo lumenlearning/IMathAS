f71bf9136ced28e6fc3092b7aef5ee4f5fcdf2b1
Michael Ackerman <michael@lumenlearning.com>
2018-05-11 09:59:40 -0700

    Removed unnecessary code.

--- ohm/models/User.php
+++ ohm/models/User.php
@@ -4,35 +4,20 @@ namespace OHM\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-require_once(__DIR__ . '/../../vendor/autoload.php');
-
 class User extends Model
 {
 
-	private $SID;
-	private $password;
-	private $rights;
-	private $FirstName;
-	private $LastName;
-	private $email;
-	private $lastaccess;
-	private $groupid;
-	private $msgnotify;
-	private $qrightsdef;
-	private $deflib;
-	private $usedeflib;
-	private $homelayout;
-	private $hasuserimg;
-	private $remoteaccess;
-	private $theme;
-	private $listperpage;
-	private $hideonpostswidget;
-	private $specialrights;
-	private $FCMtoken;
-	private $jsondata;
-	private $forcepwreset;
-
 	protected $table = "imas_users";
+
+	/**
+	 * This empty method effectively eliminates the requirement for an
+	 * updated_at column.
+	 *
+	 * @param $value
+	 */
+	public function setUpdatedAtAttribute($value)
+	{
+	}
 
 	/**
 	 * The attributes that are mass assignable.
