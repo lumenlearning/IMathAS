be4bf0d3e7efa7ed3d0aa231f8cdc70e49461574
Michael Ackerman <michael@lumenlearning.com>
2018-05-14 20:32:39 -0700

    Created LtiCredential controller.

--- ohm/api/src/configs/routes.php
+++ ohm/api/src/configs/routes.php
@@ -24,5 +24,13 @@ $app->group('/v1', function () {
 		$this->post('[/]', \OHM\Api\Controllers\GroupController::class . ':create');
 		$this->delete('/{id}', \OHM\Api\Controllers\GroupController::class . ':delete');
 		$this->put('[/{id}]', \OHM\Api\Controllers\GroupController::class . ':update');
+
+		$this->group('/{groupId}/lti_credentials', function () {
+			$this->get('[/]', \OHM\Api\Controllers\LtiCredentialController::class . ':findAll');
+			$this->get('/{id}', \OHM\Api\Controllers\LtiCredentialController::class . ':find');
+			$this->post('[/]', \OHM\Api\Controllers\LtiCredentialController::class . ':create');
+			$this->delete('/{id}', \OHM\Api\Controllers\LtiCredentialController::class . ':delete');
+			$this->put('[/{id}]', \OHM\Api\Controllers\LtiCredentialController::class . ':update');
+		});
 	});
 });
