a1b31b36f2c6ee063e8392d3c46e3f61280a3bdc
drlippman <drlippman@yahoo.com>
2018-07-02 19:40:56 -0700

    Add support for super-group templates
    (cherry picked from commit 1a457554e7d45d7d41a2aa6343fec1e490f0796e)

--- admin/actions.php
+++ admin/actions.php
@@ -530,6 +530,13 @@ switch($_POST['action']) {
 		} else if (($old_istemplate&1)==1) {
 			$istemplate |= 1;
 		}
+		if (($myspecialrights&2)==2 || $myrights==100) {
+			if (isset($_POST['issupergrptemplate'])) {
+				$istemplate |= 32;
+			}
+		} else if (($old_istemplate&32)==32) {
+			$istemplate |= 32;
+		}
 		if ($myrights==100) {
 			if (isset($_POST['isselfenroll'])) {
 				$istemplate |= 4;
