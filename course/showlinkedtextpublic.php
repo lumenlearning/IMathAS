5a745e64195e1d2f31bf59ef7a39bce1885fd077
David Lippman <drlippman@yahoo.com>
2018-11-09 22:16:52 -0800

    Show page title as h1 on showlinkedtextpublic
    (cherry picked from commit a7c1687db82dc94d244327afc65d6a9c145f35cb)
    
    Conflicts:
    	course/showlinkedtextpublic.php

--- course/showlinkedtextpublic.php
+++ course/showlinkedtextpublic.php
@@ -85,40 +85,10 @@
 	list($text,$title) = $stm->fetch(PDO::FETCH_NUM);
 	$titlesimp = strip_tags($title);
 
-	$placeinhead = '<script type="text/javascript"> $(function() {
-	$(".im_glossterm").addClass("hoverdef").each(function(i,el) {
-		$(el).attr("title",$(el).next(".im_glossdef").text());
-	   });
-	});
-	</script>';
-
 	require("../header.php");
-	echo "<div class=breadcrumb>$breadcrumbbase $titlesimp</div>";
+	echo "<div class=breadcrumb>$breadcrumbbase ".Sanitize::encodeStringForDisplay($titlesimp)."</div>";
 
-	echo '<div class="linkedtextholder" style="padding-left:10px; padding-right: 10px;">';
-	$navbuttons = '';
-	if ($coursetheme=='otbsreader.css') {
-		$now = time();
-		//DB $query = "SELECT il.id,il.title,il.avail,il.startdate,il.enddate,ii.id AS itemid ";
-		//DB $query .= "FROM imas_linkedtext as il JOIN imas_items AS ii ON il.id=ii.typeid AND ii.itemtype='LinkedText' ";
-		//DB $query .= "WHERE ii.courseid='$cid' ";
-		$query = "SELECT il.id,il.title,il.avail,il.startdate,il.enddate,ii.id AS itemid ";
-		$query .= "FROM imas_linkedtext as il JOIN imas_items AS ii ON il.id=ii.typeid AND ii.itemtype='LinkedText' ";
-		$query .= "WHERE ii.courseid=:courseid ";
-		if (!$isteacher && !$istutor) {
-			  $query .= "AND (il.avail=2 OR (il.avail=1 AND $now>il.startdate AND $now<il.enddate))";
-		}
-		$stm = $DBH->prepare($query);
-		$stm->execute(array(':courseid'=>$cid));
-		//DB $result = mysql_query($query) or die("Query failed : " . mysql_error());
-		$itemdata = array();
-		//DB while ($row = mysql_fetch_assoc($result)) {
-		while ($row = $stm->fetch(PDO::FETCH_ASSOC)) {
-			$itemdata[$row['itemid']] = $row;
-			if ($row['id']==$_GET['id']) {
-				$thisitemid = $row['itemid'];
-			}
-		}
+	echo '<div id="headershowlinkedtext" class="pagetitle"><h1>'.Sanitize::encodeStringForDisplay($titlesimp).'</h1></div>';
 
 	echo '<div style="padding-left:10px; padding-right: 10px;">';
 	echo Sanitize::outgoingHtml(filter($text));
@@ -126,7 +96,7 @@
 	if (!($_GET['from'])) {
 		echo "<div class=right><a href=\"course.php?cid=$cid\">Back</a></div>\n";
 	} else if ($fcid>0) {
-		echo "<div class=right><a href=\"{$_SERVER['HTTP_REFERER']}\">Back</a></div>\n";
+		echo "<div class=right><a href=\"" . Sanitize::url($_SERVER['HTTP_REFERER']) . "\">Back</a></div>\n";
 	} else {
 		echo "<div class=right><a href=\"public.php?cid=$cid\">Return to the Public Course Page</a></div>\n";
 	}
