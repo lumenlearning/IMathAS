21536b9b7d8a8a435be1929bfe9cb8f4c940bdb0
Michael Ackerman <michael@lumenlearning.com>
2019-12-13 11:40:26 -0800

    Updated the Desmos preview warning icon with an SVG.
    OHM-121

--- desmos/views/edit.php
+++ desmos/views/edit.php
@@ -103,7 +103,8 @@
     <div id="desmos_previewmode_buttons">
         <button id="desmos_return_to_edit_button" class="desmos button" type="button">Back to Edit</button>
         <span id="desmos_preview_warning">
-            <img id="desmos_preview_warning_image" src="/ohm/img/warning.png" alt="Warning"/>
+            <img id="desmos_preview_warning_image" src="/ohm/img/warning.svg"
+                 onerror="this.src='/ohm/img/warning.png'" alt="Warning">
             Desmos graph changes in the preview are not saved for students.
         </span>
     </div>
