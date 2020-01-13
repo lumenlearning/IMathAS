2be9983bb2a5e2911286e983b020ea9126409be0
trezp <treasure@lumenlearning.com>
2020-01-13 09:07:46 -0800

    switched modal buttons

--- desmos/views/ConfirmDesmosDelete.php
+++ desmos/views/ConfirmDesmosDelete.php
@@ -7,8 +7,8 @@
         <p>Deleting this will remove all of the content in this item. Are you sure you want to delete?<p>
     </div>
     <div class="ohm-modal-footer">
-        <button type="button" class="js-confirm-delete button">Delete</button>
         <button type="button" class="js-cancel-modal button">Cancel</button>
+        <button type="button" class="js-confirm-delete button">Delete</button>
     </div>
 </div>  
 
