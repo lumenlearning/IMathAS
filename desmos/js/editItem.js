4a275d916cd9d89d4b97e5bd6e14a67159687ca0
Michael Ackerman <michael@lumenlearning.com>
2020-02-17 13:05:56 -0800

    Fixed: "Next" and "Previous" buttons in Desmos don't work on teacher student view
    Part 1/2.
    
    OHM-339: [QA] "Next" and "Previous" buttons in Desmos don't work on teacher student view

--- desmos/js/editItem.js
+++ desmos/js/editItem.js
@@ -14,33 +14,42 @@ $(document).ready(function() {
         }
     };
 
+    /*
+     * Helper function to get a URL query parameter.
+     */
+    $.urlParam = function(name){
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        return results[1] || 0;
+    };
+
+    /*
+     * Serialize the form on the edit page, save in the user's PHP session,
+     * and redirect to the view page in preview mode.
+     */
     $("#desmos_preview_button").click(function() {
         $("#desmos_preview_button").html('Loading preview...');
         let formData = $("#desmos_item").serialize();
         $.ajax({
             type: "POST",
-            url: "/desmos/views/view.php?mode=preview",
-            data: formData,
+            url: "/course/itempreview.php?mode=store_temp_preview_data",
+            data: {
+                tempSerializedPreviewData: formData,
+            },
             success: function(data) {
-                $("#desmos_edit_container").hide();
-                $("#desmos_preview_button").html("Preview");
-                $("#desmos_preview_content").html(data);
-                window.rendermathnode();
-                $("div.mainbody").css("background-color", "#F9FAFB");
-                $("div.breadcrumb").css("background-color", "#F9FAFB");
-                $("#desmos_preview_container").show();
-                $('link[title=lux]')[0].disabled=true;
-                $('html, body').animate({ scrollTop: 0 }, "slow");
-                loadDesmos();
+                let courseId = Number($.urlParam('cid'));
+                $(location).attr('href', '/course/itempreview.php?cid=' + courseId + '&type=desmos');
             },
             error: function(data) {
-                console.log("Failed to get view page content for Desmos item.");
+                console.log("Failed to temporarily store serialized form data for Desmos interactive.");
                 console.log(data.responseText);
                 $("#preview_button").html('Preview');
             }
         });
     });
 
+    /*
+     * Unserialize form data from browser local storage, populate
+     */
     $("#desmos_return_to_edit_button").click(function() {
         $("#desmos_preview_container").hide();
         $("#desmos_preview_content").empty();
---
+++ desmos/views/preview.php
@@ -0,0 +1,45 @@
+<link rel="stylesheet" href="../themes/lux-temp.css" type="text/css" />
+<link rel="stylesheet" href="/desmos/desmos-temp.css" type="text/css" />
+<script type="text/javascript">
+    window.onload = ()=> {
+        showSteps("desmos_view_container", document.getElementById("step_list").children[0]);
+    }
+</script>
+<?php
+if ($shownav) {
+    echo '<div class="breadcrumb">'.$curBreadcrumb.'</div>';
+}
+
+// This was stored by /desmos/js/editItem.js.
+$serializedData = $_SESSION['tempSerializedPreviewData'];
+parse_str($serializedData, $desmosFormData);
+
+$item = new \Desmos\Models\DesmosItem();
+$item->setName($desmosFormData['title']);
+$item->setSummary($desmosFormData['summary']);
+// Build steps array
+$steps = [];
+foreach ($desmosFormData['step_title'] as $key => $title) {
+    $steps[$key] = [
+        "title" => $title,
+        "text" => $desmosFormData['step_text'][$key],
+        "id" => $desmosFormData['step'][$key],
+    ];
+}
+$item->setSteps($steps);
+$pagetitle = Sanitize::encodeStringForDisplay($item->name);
+
+?>
+<div id="desmos_previewmode_buttons">
+    <button id="desmos_return_to_edit_button" class="desmos button"
+            type="button">Back to Edit
+    </button>
+    <span id="desmos_preview_warning">
+        <img id="desmos_preview_warning_image" src="/ohm/img/warning.svg"
+             onerror="this.src='/ohm/img/warning.png'" alt="Warning">
+            Desmos graph changes in the preview are not saved for students.
+    </span>
+</div>
+
+<?php
+require_once(__DIR__ . '/view_content.php');
