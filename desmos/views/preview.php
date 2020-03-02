34fe80a1fcfb7abdf459d02393e99fe522359563
Angelique Weger <angelique@lumenlearning.com>
2020-03-02 18:37:55 -0500

    Clean up JS errors that occur when the steps can't be guaranteed to be on screen

--- desmos/views/preview.php
+++ desmos/views/preview.php
@@ -1,9 +1,11 @@
 <link rel="stylesheet" href="/desmos/desmos-temp.css" type="text/css" />
+<?php if (count($item->steps) > 1) { ?>
 <script type="text/javascript">
     window.onload = ()=> {
         showSteps("desmos_view_container", document.getElementById("step_list").children[0]);
     }
 </script>
+<?php } ?>
 <?php
 if ($shownav) {
     echo '<div class="breadcrumb">'.$curBreadcrumb.'</div>';
