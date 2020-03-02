5d19825831edeed61647b4799174a917bfa6fa85
Michael Ackerman <michael@lumenlearning.com>
2020-03-02 15:07:35 -0800

    Make the imas_users table migration a non-blocking operation.
    OHM-344: Bring in MOM changes up through 2/26

--- migrations/20200128_add_email_index.php
+++ migrations/20200128_add_email_index.php
@@ -3,7 +3,8 @@
 //Add additional indexes
 $DBH->beginTransaction();
 
- $query = "ALTER TABLE  `imas_users` ADD INDEX ( `email` )";
+ // OHM-specific change: Make this a non-blocking operation. (algorithm=inplace)
+ $query = "ALTER TABLE  `imas_users` ADD INDEX ( `email` ), ALGORITHM=INPLACE, LOCK=NONE";
  $res = $DBH->query($query);
  if ($res===false) {
  	 echo "<p>Query failed: ($query) : " . $DBH->errorInfo() . "</p>";
