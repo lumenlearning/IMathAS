ae6119163b3933c8a8f0dcfd1e1aaea9b7575621
Alena Holligan <alena@lumenlearning.com>
2020-03-06 11:22:42 -0800

    ohm-435 update teacher text

--- ohm/views/banner/teachers.php
+++ ohm/views/banner/teachers.php
@@ -4,23 +4,14 @@
         <h3>OHM Desmos Interactives are here!</h3>
 
         <p>
-            We’re excited to announce the release of Desmos Interactives, a new
-            item type in OHM to support student learning and mastery of graphing
-            concepts, leveraging the accessible and easy to use Desmos graphing
-            calculator.
+            We’re excited to announce the release of Desmos Interactives,
+            a new item type to support student learning and mastery of graphing concepts.
+            Along with this platform feature release,
+            we’re sharing a complete collection of College Algebra Desmos Interactives.
         </p>
         <p>
-            Along with this platform feature release, we’re sharing a complete
-            collection of College Algebra Desmos Interactives for faculty to
-            either use as is or modify to best suit their students’ needs.
-        </p>
-        <p>
-            Check out our
-            <a target="_blank" href="https://info.lumenlearning.com/ohmdesmos">info page</a>
-            for details on how to explore Desmos
-            Interactives. Learn more about our partnership with Desmos, product
-            plans, or provide feedback
-            <a target="_blank" href="https://lumenlearning.com/exciting-partner-plans-lumen-desmos/">here</a>.
+            For more details, check out our
+            <a target="_blank" href="https://info.lumenlearning.com/ohmdesmos">info page</a>.
         </p>
     </div>
 </div>
