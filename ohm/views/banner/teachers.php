2bf38de5514606f8b34ca48b7627a12f7e59d85a
Alena Holligan <alena@lumenlearning.com>
2020-03-09 11:20:24 -0700

    ohm-435 update teacher banner header

--- ohm/views/banner/teachers.php
+++ ohm/views/banner/teachers.php
@@ -1,8 +1,6 @@
 <div class="ohm-course-banner">
-    <h2 class="ohm-course-banner-header">OHM Updates</h2>
+    <h2 class="ohm-course-banner-header">OHM Desmos Interactives are here!</h2>
     <div class="ohm-course-banner-content">
-        <h3>OHM Desmos Interactives are here!</h3>
-
         <p>
             We’re excited to announce the release of Desmos Interactives,
             a new item type to support student learning and mastery of graphing concepts.
