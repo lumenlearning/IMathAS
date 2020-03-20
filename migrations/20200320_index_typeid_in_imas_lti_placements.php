ee19facf8fa7abf746b113cbb3a0bcd108614649
Alena Holligan <alena@lumenlearning.com>
2020-03-20 11:32:09 -0700

    ohm-504 add index to imas_lti_placements.typeid

---
+++ migrations/20200320_index_typeid_in_imas_lti_placements.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * OHM: imas_lti_placements
+ * Add index on typeid column
+ */
+$DBH->beginTransaction();
+
+$query = "ALTER TABLE `imas_lti_placements`
+    ADD INDEX `typeid` (`typeid` ASC),
+      ALGORITHM=INPLACE,
+      LOCK=NONE
+";
+$res = $DBH->query($query);
+if ($res === false) {
+    echo "<p>Query failed: ($query) : ";
+    var_dump($DBH->errorInfo());
+    echo  "</p>";
+    $DBH->rollBack();
+    return false;
+}
+
+$DBH->commit();
+
+echo "<p style='color: green;'>✓ Added index on typeid columns to table: imas_lti_placements</p>";
+
+return true;
