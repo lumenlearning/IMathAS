c11ab2d5aa346943c5d2250caa266a5a287b322a
Michael Ackerman <michael@lumenlearning.com>
2020-03-24 11:27:53 -0700

    OHM-494: Dismiss banner option
    Updated /course/course.php for banner dismissal functionality.

--- ohm-hooks/banner.php
+++ ohm-hooks/banner.php
@@ -4,8 +4,10 @@
  * This allows us to keep state and display banners only once on the same page.
  *
  * To set which banner is displayed to users, modify the banner ID where
- * the displayBanner() function is being called. As of 2020 Mar 24, look
- * in /index.php.
+ * the displayBanner() function is being called.
+ * As of 2020 Mar 24, look in:
+ *   - /index.php
+ *   - /course/course.php
  *
  * Notes / history:
  *   - https://lumenlearning.atlassian.net/browse/OHM-400
