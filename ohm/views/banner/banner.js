0eefdd8b1c62ab74f7caf5dda02611fedf0a8022
Angelique Weger <angelique@lumenlearning.com>
2020-03-27 10:14:36 -0400

    Use aria-controls to pass banner details

--- ohm/views/banner/banner.js
+++ ohm/views/banner/banner.js
@@ -1,18 +1,20 @@
 window.onload = function () {
-  $("#js-dismiss-banner").click(function () {
-    let bannerId = $(".course-banner").attr('data-banner-id');
-    dismissNotice(bannerId);
+  $("#js-dismiss-banner").click(function (event) {
+    event.preventDefault();
+    let bannerEl = $(this).attr("aria-controls");
+    let bannerId = $(banner).attr("data-banner-id");
+    dismissNotice(bannerId, bannerEl);
   });
 }
 
-function dismissNotice(id) {
+function dismissNotice(id, el) {
   $.ajax({
     method: "POST",
     url: "/ohm/dismiss_notice.php",
     data: { "notice-id": id }
   })
     .done(function (msg) {
-      $(".course-banner").slideUp();
+      $(el).slideUp();
     })
     .fail(function (xhr, status) {
       console.log("Failed to dismiss notice. Error status: " + status);
