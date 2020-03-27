73a8f736fa9be62437d5f09f26e85d30eb79ea7e
Angelique Weger <angelique@lumenlearning.com>
2020-03-27 09:47:07 -0400

    Use SVG for close icon

--- ohm/views/banner/students.php
+++ ohm/views/banner/students.php
@@ -6,7 +6,20 @@
             class="u-button-reset course-banner-close-button"
             id="js-dismiss-banner"
             type="button">
-            [x]
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="12" height="12" role="img" aria-hidden="true">
+                <defs>
+                    <path d="M7.414 6l4.293-4.293A.999.999 0 1 0 10.293.293L6 4.586 1.707.293A.999.999 0 1 0 .293 1.707L4.586 6 .293 10.293a.999.999 0 1 0 1.414 1.414L6 7.414l4.293 4.293a.997.997 0 0 0 1.414 0 .999.999 0 0 0 0-1.414L7.414 6z" id="icon-close-a"/>
+                </defs>
+                <g fill="none" fill-rule="evenodd">
+                    <mask id="icon-close-b" fill="#fff">
+                       <use xlink:href="#icon-close-a"/>
+                    </mask>
+                    <use fill="#637381" xlink:href="#icon-close-a"/>
+                    <g mask="url(#icon-close-b)" fill="#212B36">
+                       <path d="M0 0h12v12H0z"/>
+                    </g>
+                </g>
+            </svg>
         </button>
     </header>
     <div class="course-banner-content">
