6f9b81fb42cf651b23e60defb083978bde355897
drlippman <drlippman@yahoo.com>
2020-03-31 18:20:53 -0700

    Bug fix editor issues in new assess
    1) essay answers weren't triggering autosave on blur
    
    2) Essay entries show show disabled on showwork page
    
    This also addresses failure to rerender on returning to a question in some cases.
    IM-719
    
    (cherry picked from commit 471e49ca76eec5539eb5a2fa940a8a2f33470ec6)

--- assess2/vue-src/src/components/question/Question.vue
+++ assess2/vue-src/src/components/question/Question.vue
@@ -427,7 +427,7 @@ export default {
       window.$(this.$refs.thisqwrap).find('select.ansyel').after(svgychk);
       window.$(this.$refs.thisqwrap).find('select.ansred').after(svgx);
 
-      actions.setRendered(this.qn);
+      actions.setRendered(this.qn, true);
     },
     setInitValues () {
       var regex = new RegExp('^(qn|tc|qs)\\d');
@@ -498,6 +498,9 @@ export default {
       this.disableOutOfTries();
       this.renderAndTrack();
     }
+  },
+  beforeDestroy () {
+    actions.setRendered(this.qn, false);
   },
   watch: {
     active: function (newVal, oldVal) {
