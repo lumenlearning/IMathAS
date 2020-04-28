40ecbffa34bb17dc725a5f585cdf13d04b83a587
trezp <treasure@lumenlearning.com>
2020-04-28 10:29:46 -0700

    change assessment checkbox to radio buttons

--- ohm/js/newPlayerDefault.js
+++ ohm/js/newPlayerDefault.js
@@ -1,1 +1,8 @@
-console.log("KILL MEEEEEEEE")
+$(document).ready(function(){
+    let assessVersion = $('input:radio[name=assess-version]:checked').val();
+    $('.js-assessmentVersionText').text(assessVersion);
+
+    $('.js-version-inputs').on('change', 'input', function(event){
+        $('.js-assessmentVersionText').text(event.target.value);
+    });
+});
