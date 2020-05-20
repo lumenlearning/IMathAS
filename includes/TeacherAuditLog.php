f32156aa53d1208080fbd4675f331d66b34462a8
Alena Holligan <alena@lumenlearning.com>
2020-05-20 10:08:54 -0700

    ohm-525 update typing and documentation for class

--- includes/TeacherAuditLog.php
+++ includes/TeacherAuditLog.php
@@ -7,6 +7,9 @@ class TeacherAuditLog
 {
     const STUDENTS = [10];
     const TEACHERS = [20,40,75,100];
+    /**
+     * Maps to ENUM
+     */
     const ACTIONS = [
         "Assessment Settings Change",
         "Mass Assessment Settings Change",
@@ -19,9 +22,15 @@ class TeacherAuditLog
         "Change Grades"
     ];
 
-    public static function addTracking($courseid, $action, $itemid = null, $metadata = array(), ?PDO $dbhOverride = null)
+    /**
+     * @param  int      $courseid resolves to imas_courses.id
+     * @param  string   $action   must match the ACTIONS const
+     * @param  int|null $itemid   depends on the "action" of tracking
+     * @param  array    $metadata extra details to store such as grade
+     * @return bool whether tracking has been added or not
+     */
+    public static function addTracking(int $courseid, string $action, ?int $itemid = null, array $metadata = []): bool
     {
-        $dbh = is_null($dbhOverride) ? $GLOBALS['DBH'] : $dbhOverride;
         if (!in_array($action, self::ACTIONS)) {
             //log exception
             return false;
@@ -40,16 +49,27 @@ class TeacherAuditLog
             ':metadata' => json_encode($metadata)
         ));
     }
-    public static function findActionsByCourse($cid, ?PDO $dbhOverride = null): array
+
+    /**
+     * @param  int $cid resolves to imas_courses.id
+     * @return array associative array of logs
+     */
+    public static function findActionsByCourse(int $cid): array
     {
-        $dbh = is_null($dbhOverride) ? $GLOBALS['DBH'] : $dbhOverride;
         $query = "SELECT id, userid, courseid, action, itemid, metadata, created_at FROM imas_teacher_audit_log "
             . "WHERE courseid=? ORDER BY created_at DESC";
         $stm = $GLOBALS['DBH']->prepare($query);
         $stm->execute([$cid]);
         return $stm->fetchAll(\PDO::FETCH_ASSOC);
     }
-    public static function findCourseItemAction($cid, $itemid, $action)
+
+    /**
+     * @param  int $cid    resolves to imas_courses.id
+     * @param  int $itemid depends on the "action" of tracking
+     * @param  int $action must match the ACTIONS const
+     * @return array associative array of logs
+     */
+    public static function findCourseItemAction(int $cid, int $itemid, int $action): array
     {
         $query = "SELECT id, userid, courseid, action, itemid, metadata, created_at FROM imas_teacher_audit_log "
             . "WHERE courseid=? AND itemid=? AND action=? ORDER BY created_at DESC";
@@ -61,9 +81,14 @@ class TeacherAuditLog
         ]);
         return $stm->fetchAll(\PDO::FETCH_ASSOC);
     }
-    public static function findCourseAction($cid, $action, ?PDO $dbhOverride = null): array
+
+    /**
+     * @param  int    $cid    resolves to imas_courses.id
+     * @param  string $action must match the ACTIONS const
+     * @return array associative array of logs
+     */
+    public static function findCourseAction(int $cid, string $action): array
     {
-        $dbh = is_null($dbhOverride) ? $GLOBALS['DBH'] : $dbhOverride;
         $query = "SELECT id, userid, courseid, action, itemid, metadata, created_at FROM imas_teacher_audit_log "
             . "WHERE courseid=? AND action=? ORDER BY created_at DESC";
         $stm = $GLOBALS['DBH']->prepare($query);
@@ -73,48 +98,53 @@ class TeacherAuditLog
         ]);
         return $stm->fetchAll(\PDO::FETCH_ASSOC);
     }
-    public static function countActionsByCourse(array $cid, array $actions, ?PDO $dbhOverride = null): array
-    {
-        $dbh = is_null($dbhOverride) ? $GLOBALS['DBH'] : $dbhOverride;
 
+    /**
+     * @param  array $cid     resolves to imas_courses.id
+     * @param  array $actions must match the ACTIONS const
+     * @return array associative array of course action counts with courseid as key
+     */
+    public static function countActionsByCourse(array $cid, array $actions): array
+    {
         $ph1 = \Sanitize::generateQueryPlaceholders($cid);
         $ph2 = \Sanitize::generateQueryPlaceholders($actions);
-        $query = "SELECT courseid, action, created_at, count(action) as itemcount FROM imas_teacher_audit_log "
-            . "WHERE courseid in ($ph1) AND action in ($ph2) GROUP BY courseid, action ORDER BY created_at DESC";
-        $stm = $dbh->prepare($query);
+        $query = "SELECT courseid, action, count(action) as itemcount FROM imas_teacher_audit_log "
+            . "WHERE courseid in ($ph1) AND action in ($ph2) GROUP BY courseid, action";
+        $stm = $GLOBALS['DBH']->prepare($query);
         $stm->execute(array_merge($cid,$actions));
 
         $courses = array();
         while ($row = $stm->fetch(\PDO::FETCH_ASSOC)) {
             $courses[$row['courseid']]['courseid'] = $row['courseid'];
-            if ($row['created_at'] > $courses[$row['courseid']]['lastactivity']) {
-                $courses[$row['courseid']]['lastactivity'] = $row['created_at'];
-            }
             $action = substr($row['action'], strpos($row['action'], " ") + 1);
             $courses[$row['courseid']][$action] = $row['itemcount'];
         }
         return $courses;
     }
+
+    /**
+     * @param  array      $actions        must match the ACTIONS const
+     * @param  DateTime   $startTimestamp starting date range
+     * @param  DateTime   $endTimestamp   ending date range
+     * @param  array|null $teacher        imas_users.id of teacher
+     * @return array associative array of teacher actions with teacher userid as key
+     */
     public static function countActionsByTeacher(
         array $actions,
         DateTime $startTimestamp,
         DateTime $endTimestamp,
-        ?array $teacher = null,
-        ?PDO $dbhOverride = null
+        ?array $teacher = null
     ): array
     {
-        $dbh = is_null($dbhOverride) ? $GLOBALS['DBH'] : $dbhOverride;
-
         $ph = \Sanitize::generateQueryPlaceholders($actions);
         $query = "SELECT g.name, u.FirstName, u.LastName, l.userid, l.action, count(l.action) as itemcount 
             FROM imas_teacher_audit_log as l JOIN imas_users as u ON l.userid = u.id
             LEFT JOIN imas_groups AS g ON u.groupid=g.id
             WHERE l.action in ($ph) AND l.created_at >= ? AND l.created_at <= ? 
             GROUP BY l.userid, l.action";
-        $stm = $dbh->prepare($query);
+        $stm = $GLOBALS['DBH']->prepare($query);
         $params = array_merge($actions, [$startTimestamp->format("Y-m-d H:i:s"),$endTimestamp->format("Y-m-d H:i:s")]);
 
-        $stm = $dbh->prepare($query);
         $stm->execute($params);
 
         $teachers = array();
