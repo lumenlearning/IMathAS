4312ef981c4de6b9cd1cf1ea1195841172010d6c
drlippman <drlippman@yahoo.com>
2020-09-12 09:51:12 -0700

    Replace "with score" with "after last try"
    And adjust behavior.
    
    (cherry picked from commit 60dc62621fc50af16b34a4556aa50b7f9b6592b4)

--- course/chgassessments2form.php
+++ course/chgassessments2form.php
@@ -820,7 +820,7 @@ var app = new Vue({
 			};
 			var with_score = {
 				'value': 'with_score',
-				'text': '<?php echo _('Show with the score'); ?>'
+				'text': '<?php echo _('After the last try on a question');?>'
 			};
 
 			var out = [];
