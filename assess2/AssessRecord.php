649c116ea92f64503185a9561384250520a8721a
drlippman <drlippman@yahoo.com>
2020-10-18 10:25:40 -0700

    Get scaffolded questions to show all parts in teacher preview
    (cherry picked from commit 982052be83260766c3cb58ea606d7464780d76fc)

--- assess2/AssessRecord.php
+++ assess2/AssessRecord.php
@@ -1868,6 +1868,9 @@ class AssessRecord
         $seqPartDone[$pn] = ($partattemptn[$pn] > 0);
       }
     }
+    if ($numParts == 0 && $this->teacherInGb) {
+        $seqPartDone = true;
+    }
     $attemptn = (count($partattemptn) == 0) ? 0 : max($partattemptn);
     $questionParams = new QuestionParams();
     $questionParams
