c70079f116e13a94c00c689c97d9e3e3b0a98e54
drlippman <drlippman@yahoo.com>
2021-06-06 11:07:35 -0700

    Include excused marker in GB export
    (cherry picked from commit d361bac0048c0c13cbc468b100dbcde413a4d5d1)

--- course/gb-export.php
+++ course/gb-export.php
@@ -566,7 +566,7 @@ function gbinstrdisp() {
 						} else {
 							echo '<sup>e</sup>';
 						}
-					}
+                    }
 				} else if ($gbt[0][1][$j][6]==1) { //offline
 
 					if (isset($gbt[$i][1][$j][0])) {
@@ -587,6 +587,9 @@ function gbinstrdisp() {
 					} else {
 						echo '-';
 					}
+                }
+                if (!empty($gbt[$i][1][$j][14])) { //excused
+					echo '<sup>x</sup>';
 				}
 				if (isset($gbt[$i][1][$j][5]) && ($gbt[$i][1][$j][5]&(1<<$availshow)) && !$hidepast) {
 					echo '<sub>d</sub></span>';
