3d0d47d4fe4da2e26943f5c4fb5008a1b523bc2c
kittysnacks <25309928+kittysnacks@users.noreply.github.com>
2021-08-17 17:08:46 -0700

    OHM-1035: FullStory - Pass Role (#681)
    Provide user role to FullStory for authenticated users.
    
    OHM-1035: FullStory - Pass Role

--- ohm/tests/unit/ohm/tracking/FullStoryTest.php
+++ ohm/tests/unit/ohm/tracking/FullStoryTest.php
@@ -19,6 +19,66 @@ final class FullStoryTest extends TestCase
         $this->fullStory = new FullStory();
 
         putenv('FULLSTORY_ENABLED=true');
+
+        $GLOBALS['configEnvironment'] = 'test';
+        $GLOBALS['myrights'] = 10; // 10 = student
+    }
+
+    /*
+     * isFullStoryEnabled
+     */
+    public function testIsFullStoryEnabled_Enabled(): void
+    {
+        $result = $this->fullStory::isFullStoryEnabled();
+        $this->assertTrue($result);
+    }
+
+    public function testIsFullStoryEnabled_Disabled(): void
+    {
+        putenv('FULLSTORY_ENABLED'); // This unsets the environment variable.
+
+        $result = $this->fullStory::isFullStoryEnabled();
+        $this->assertFalse($result);
+    }
+
+    /*
+     * isDebugMarkupEnabled
+     */
+
+    public function testIsDebugMarkupEnabled_Yes(): void
+    {
+        $GLOBALS['configEnvironment'] = 'test';
+        $GLOBALS['myrights'] = 100;
+
+        $result = $this->fullStory::isDebugMarkupEnabled();
+        $this->assertTrue($result);
+    }
+
+    public function testIsDebugMarkupEnabled_Production(): void
+    {
+        $GLOBALS['configEnvironment'] = 'production';
+        $GLOBALS['myrights'] = 100;
+
+        $result = $this->fullStory::isDebugMarkupEnabled();
+        $this->assertFalse($result);
+    }
+
+    public function testIsDebugMarkupEnabled_Unauthenticated(): void
+    {
+        $GLOBALS['configEnvironment'] = 'test';
+        $GLOBALS['myrights'] = 0;
+
+        $result = $this->fullStory::isDebugMarkupEnabled();
+        $this->assertFalse($result);
+    }
+
+    public function testIsDebugMarkupEnabled_Invalid(): void
+    {
+        $GLOBALS['configEnvironment'] = 'test';
+        $GLOBALS['myrights'] = '10'; // String values are considered invalid.
+
+        $result = $this->fullStory::isDebugMarkupEnabled();
+        $this->assertFalse($result);
     }
 
     /*
@@ -49,11 +109,102 @@ final class FullStoryTest extends TestCase
         $this->assertNotEmpty($result);
     }
 
-    public function testGetHeaderSnippet_Disabled(): void
-    {
-        putenv('FULLSTORY_ENABLED'); // This unsets the environment variable.
+    /*
+     * getDebugMarkupSnippet
+     */
 
-        $result = $this->fullStory::getHeaderSnippet();
+    public function testGetDebugMarkupSnippet(): void
+    {
+        $result = $this->fullStory::getDebugMarkupSnippet();
+        $this->assertNotEmpty($result);
+    }
+
+    /*
+     * getUserMetadataSnippet
+     */
+
+    public function testGetUserMetadataFullStory(): void
+    {
+        $result = $this->fullStory::getUserMetadataSnippet();
+        $this->assertNotEmpty($result);
+    }
+
+    public function testGetUserMetadataFullStory_RightsNotSet(): void
+    {
+        unset($GLOBALS['myrights']);
+
+        $result = $this->fullStory::getUserMetadataSnippet();
         $this->assertEmpty($result);
+    }
+
+    public function testGetUserMetadataFullStory_NullRights(): void
+    {
+        $GLOBALS['myrights'] = null;
+
+        $result = $this->fullStory::getUserMetadataSnippet();
+        $this->assertEmpty($result);
+    }
+
+    public function testGetUserMetadataFullStory_EmptyRights(): void
+    {
+        $GLOBALS['myrights'] = '';
+
+        $result = $this->fullStory::getUserMetadataSnippet();
+        $this->assertEmpty($result);
+    }
+
+    public function testGetUserMetadataFullStory_Zero(): void
+    {
+        $GLOBALS['myrights'] = '0';
+
+        $result = $this->fullStory::getUserMetadataSnippet();
+        $this->assertEmpty($result);
+    }
+
+    /*
+     * getUserRole
+     */
+
+    public function testgetUserRole(): void
+    {
+        $GLOBALS['myrights'] = 1;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('unknown', $role);
+
+        $GLOBALS['myrights'] = 666;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('unknown', $role);
+
+        $GLOBALS['myrights'] = 5;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('guest', $role);
+
+        $GLOBALS['myrights'] = 10;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('student', $role);
+
+        $GLOBALS['myrights'] = 12;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('pending-approval', $role);
+
+        $GLOBALS['myrights'] = 15;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('tutor', $role);
+
+        $GLOBALS['myrights'] = 20;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('instructor', $role);
+
+        $GLOBALS['myrights'] = 40;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('limited-course-creator', $role);
+
+        $GLOBALS['myrights'] = 75;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('group-admin', $role);
+
+        $GLOBALS['myrights'] = 100;
+        $role = $this->fullStory::getUserRole();
+        $this->assertEquals('administrator', $role);
     }
 }
--- ohm/tracking/FullStory.php
+++ ohm/tracking/FullStory.php
@@ -16,18 +16,53 @@ class FullStory
      */
     public static function outputHeaderSnippet(): bool
     {
-        $snippet = self::getHeaderSnippet();
-
-        if ('' == $snippet) {
+        if (!self::isFullStoryEnabled()) {
             return false;
-        } else {
-            echo $snippet;
-            return true;
         }
+
+        // FullStory JS snippet.
+        echo self::getHeaderSnippet();
+
+        // User metadata for FullStory.
+        echo self::getUserMetadataSnippet();
+
+        // For dev / QA / debugging.
+        if (self::isDebugMarkupEnabled()) {
+            echo self::getDebugMarkupSnippet();
+        }
+
+        return true;
     }
 
     /**
-     * Get the JS snippet required to load FullStory during a user's
+     * Determine if FullStory is enabled in OHM.
+     *
+     * @return bool True if enabled. False if not.
+     */
+    public static function isFullStoryEnabled(): bool
+    {
+        return 'true' == getenv('FULLSTORY_ENABLED');
+    }
+
+    /**
+     * Determine if PII should be marked with visual indicators for
+     * dev / debugging purposes.
+     *
+     * Current requirements:
+     *   - The environment variable `CONFIG_ENV` is NOT `production`.
+     *   - The currently logged in user is an administrator. (Rights == 100)
+     *
+     * @return bool True if debug markup should be displayed. False if not.
+     */
+    public static function isDebugMarkupEnabled(): bool
+    {
+        global $myrights, $configEnvironment;
+
+        return 100 == $myrights && 'production' != $configEnvironment;
+    }
+
+    /**
+     * Get the HTML snippet required to load FullStory during a user's
      * session.
      *
      * This only returns HTML elements as a string. It does not output
@@ -36,28 +71,102 @@ class FullStory
      * This string should be inserted into the <head> section of a web page.
      *
      * @return string An HTML snippet required to load FullStory on a page.
-     *                An empty string if FullStory is not enabled.
      */
     public static function getHeaderSnippet(): string
     {
-        global $basesiteurl, $myrights, $configEnvironment;
+        global $basesiteurl;
 
-        if ('true' != getenv('FULLSTORY_ENABLED')) {
+        return sprintf(
+            '<script src="%s/ohm/js/fullstory.js"></script>' . "\n",
+            $basesiteurl);
+    }
+
+    /**
+     * Get the HTML snippet that enables marking up PII for
+     * dev / QA / debugging purposes.
+     *
+     * This assists QA & devs to ensure sensitive user information s properly
+     * excluded or masked during non-production testing.
+     *
+     * @return string An HTML snippet that enables marking up PII.
+     */
+    public static function getDebugMarkupSnippet(): string
+    {
+        global $basesiteurl;
+
+        return sprintf(
+            '<link rel="stylesheet" href="%s/ohm/tracking/sensitive_info_highlight.css">' . "\n",
+            $basesiteurl);
+    }
+
+    /**
+     * Get the HTML that provides user metadata to FullStory.
+     *
+     * @return string An HTML snippet that provides user metadata to FullStory.
+     *                An empty string for unauthenticated users.
+     */
+    public static function getUserMetadataSnippet(): string
+    {
+        global $myrights;
+
+        // We can only provide user metadata for authenticated users.
+        if (empty($myrights) || 0 == $myrights) {
             return '';
         }
 
-        $snippet = sprintf(
-            '<script src="%s/ohm/js/fullstory.js"></script>' . "\n",
-            $basesiteurl);
+        $roleName = self::getUserRole();
 
-        // This assists QA & devs to ensure sensitive user information
-        // is properly excluded or masked during non-production testing.
-        if (100 == $myrights && 'production' != $configEnvironment) {
-            $snippet .= sprintf(
-                '<link rel="stylesheet" href="%s/ohm/tracking/sensitive_info_highlight.css">' . "\n",
-                $basesiteurl);
+        return sprintf('<script type="text/javascript">
+  if ("undefined" !== FS && null != FS) {
+    FS.setUserVars({
+     "role_str" : "%s",
+    });
+  }
+</script>' . "\n",
+            $roleName);
+    }
+
+    /**
+     * Get a user's role as a string. (10 = "student", etc)
+     *
+     * @return string A user's role name. "none" for unauthenticated users.
+     */
+    public static function getUserRole(): string
+    {
+        global $myrights;
+
+        if (empty($myrights) || 0 == $myrights) {
+            return 'none';
         }
 
-        return $snippet;
+        $roleName = 'unknown';
+        switch ($myrights) {
+            case 5:
+                $roleName = 'guest';
+                break;
+            case 10:
+                $roleName = 'student';
+                break;
+            case 12:
+                $roleName = 'pending-approval';
+                break;
+            case 15:
+                $roleName = 'tutor';
+                break;
+            case 20:
+                $roleName = 'instructor';
+                break;
+            case 40:
+                $roleName = 'limited-course-creator';
+                break;
+            case 75:
+                $roleName = 'group-admin';
+                break;
+            case 100:
+                $roleName = 'administrator';
+                break;
+        }
+
+        return $roleName;
     }
 }
