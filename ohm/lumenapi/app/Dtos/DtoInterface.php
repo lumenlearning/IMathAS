d8460839e52ca1ac5d6b40a75c32ce89177a0cd2
Michael Ackerman <michael@lumenlearning.com>
2021-10-20 17:58:50 -0700

    Updated DtoInterface.
    OHM-1083: Create imas_student endpoint in OHM

--- ohm/lumenapi/app/Dtos/DtoInterface.php
+++ ohm/lumenapi/app/Dtos/DtoInterface.php
@@ -4,4 +4,10 @@ namespace App\Dtos;
 
 interface DtoInterface
 {
+    /**
+     * Return all fields as an associative array.
+     *
+     * @return array An associative array containing all DTO fields.
+     */
+    public function toArray(): array;
 }
