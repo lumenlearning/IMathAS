12dd6857a46e970415f6c173ee5ac681e1c2a84b
Michael Ackerman <michael@lumenlearning.com>
2021-10-22 09:56:58 -0700

    Updated interfaces.
    OHM-1083: Create imas_student endpoint in OHM

--- ohm/lumenapi/app/Repositories/Interfaces/EnrollmentRepositoryInterface.php
+++ ohm/lumenapi/app/Repositories/Interfaces/EnrollmentRepositoryInterface.php
@@ -2,7 +2,13 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Models\Enrollment;
+
 interface EnrollmentRepositoryInterface
 {
     public function getById(int $enrollmentId);
+
+    public function getAll(int $limit, int $offset, array $columnFilters);
+
+    public function update(Enrollment $enrollment);
 }
--- ohm/lumenapi/app/Services/Interfaces/EnrollmentServiceInterface.php
+++ ohm/lumenapi/app/Services/Interfaces/EnrollmentServiceInterface.php
@@ -4,5 +4,9 @@ namespace App\Services\Interfaces;
 
 interface EnrollmentServiceInterface
 {
+    public function getAll(int $pageNum, int $pageSize, array $filters): array;
+
     public function getById(int $id): ?array;
+
+    public function updateById(int $id, array $input): array;
 }
