1bd75026d818687bd9025255e1f71bf80ee46b3d
Michael Ackerman <michael@lumenlearning.com>
2022-02-18 11:31:28 -0800

    Move Lumen framework scripts from postdeploy to prebuild.
    This hopefully addresses the issue of these scripts not being
    run on AWS Beanstalk configuration changes.
    
    ETH-331: OHM question API is returning HTTP status 500

--- .platform/hooks/prebuild/10-lumenapi_packages.sh
+++ .platform/hooks/prebuild/10-lumenapi_packages.sh
@@ -3,6 +3,6 @@
 export COMPOSER_HOME=/tmp/.composer
 
 echo "Installing composer dependencies for Lumen API. (OHM question API)"
-cd /var/app/current/ohm/lumenapi
+cd ohm/lumenapi
 /usr/bin/php /usr/bin/composer.phar install
 
