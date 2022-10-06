dd16041ee13ac2fd75aa5d81c88f097fc834f063
Michael Ackerman <michael@lumenlearning.com>
2022-10-06 11:02:53 -0700

    Updated tests.
    AST-229: Update OHM question feedback json structure for single and multipart questions

--- ohm/lumenapi/tests/unit/Controllers/QuestionControllerTest.php
+++ ohm/lumenapi/tests/unit/Controllers/QuestionControllerTest.php
@@ -111,6 +111,90 @@ class QuestionControllerTest extends TestCase
         'varscore' => '0',
     ];
 
+    private array $imasQuestionSet_dbRow_multipart = [
+        'id' => '3609',
+        'uniqueid' => '1665009644605959',
+        'adddate' => '1665009644',
+        'lastmoddate' => '1665018423',
+        'ownerid' => '1',
+        'author' => '<h1>AdminLastName</h1>,<h1>AdminFirstName</h1>',
+        'userrights' => '0',
+        'license' => '1',
+        'description' => 'Multipart, multiple inputs per part',
+        'qtype' => 'multipart',
+        'control' => 'loadlibrary("ohm_macros")
+
+$anstypes = "choices,choices,choices,number"
+
+// Part 1 - Choose the best color
+$choices[0] = array("Purple", "Red", "Blue", "Green")
+$answer[0] = "0"
+$colorfeedbacks[0] = "Excellent choice."
+$colorfeedbacks[1] = "Nope."
+$colorfeedbacks[2] = "Try again."
+$colorfeedbacks[3] = "Not even close."
+
+// Part 2 - Cup storage. Are you an upper or a downer?
+$choices[1] = array("Up", "Down", "Sideways")
+$answer[1] = "2"
+$numbersfeedbacks[0] = "Nope!"
+$numbersfeedbacks[1] = "No."
+$numbersfeedbacks[2] = "Correct."
+
+// Part 3 - Is Hawaiian a valid pizza option?
+$choices[2] = array("Yes", "No", "Sometimes", "Only on Tuesdays")
+$answer[2] = "0"
+$pizzafeedbacks = array(
+  "Correct. Hawaiian pizza is always valid pizza.",
+  "Incorrect.",
+  "Try again.",
+  "All days are acceptable pizza days."
+)
+
+// Part 4 - What is the answer to life, the universe, and everything?
+$answer[3] = "42"
+
+$feedback = mergearrays(
+  ohm_getfeedbacktxt($stuanswer[$thisq], $colorfeedbacks, $answer[0], 0),
+  ohm_getfeedbacktxt($stuanswer[$thisq], $numbersfeedbacks, $answer[1], 1),
+  ohm_getfeedbacktxt($stuanswer[$thisq], $pizzafeedbacks, $answer[2], 2),
+  ohm_getfeedbackbasic("Correct!", "Not correct.", $thisq, 3)
+)',
+        'qcontrol' => '',
+        'qtext' => 'Choose the best color:
+$answerbox[0]
+
+Cup storage. Are you an upper or a downer?
+$answerbox[1]
+
+Is Hawaiian a valid pizza option?
+$answerbox[2]
+
+What is the answer to life, the universe, and everything?
+$answerbox[3]
+',
+        'answer' => '',
+        'solution' => '',
+        'extref' => '',
+        'hasimg' => '0',
+        'deleted' => '0',
+        'avgtime' => '0',
+        'ancestors' => '',
+        'ancestorauthors' => '',
+        'otherattribution' => '',
+        'importuid' => '',
+        'replaceby' => '0',
+        'broken' => '0',
+        'solutionopts' => '6',
+        'sourceinstall' => '',
+        'meantimen' => '0',
+        'meantime' => '0',
+        'vartime' => '0',
+        'meanscoren' => '0',
+        'meanscore' => '0',
+        'varscore' => '0'
+    ];
+
     public function setUp(): void
     {
         if (!$this->app) {
@@ -215,7 +299,7 @@ class QuestionControllerTest extends TestCase
         $this->assertNull($scoreResponse['correctAnswers']['answers']);
     }
 
-    public function testGetQuestion_withFeedback(): void
+    public function testGetQuestion_withFeedback_singlePart(): void
     {
         // Setup mocks.
         $this->questionSetRepository
@@ -237,14 +321,63 @@ class QuestionControllerTest extends TestCase
         $this->assertEquals(4120, $responseData['seed']);
         $this->assertEquals([], $responseData['errors']);
         $this->assertNotEmpty($responseData['feedback']);
-        $this->assertEquals('correct', $responseData['feedback'][0]['correctness']);
-        $this->assertEquals('This is correct. Way to go.', $responseData['feedback'][0]['feedback']);
-        $this->assertEquals('incorrect', $responseData['feedback'][1]['correctness']);
-        $this->assertEquals('Sorry, Option B is incorrect. Try again.', $responseData['feedback'][1]['feedback']);
-        $this->assertEquals('incorrect', $responseData['feedback'][2]['correctness']);
-        $this->assertEquals('Sorry, Option C is not the right answer. Try again.', $responseData['feedback'][2]['feedback']);
-        $this->assertEquals('incorrect', $responseData['feedback'][3]['correctness']);
-        $this->assertEquals('Sorry, Option D was the wrong choice. Try again.', $responseData['feedback'][3]['feedback']);
+        $this->assertEquals('correct', $responseData['feedback']['qn0-0']['correctness']);
+        $this->assertEquals('This is correct. Way to go.', $responseData['feedback']['qn0-0']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn0-1']['correctness']);
+        $this->assertEquals('Sorry, Option B is incorrect. Try again.', $responseData['feedback']['qn0-1']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn0-2']['correctness']);
+        $this->assertEquals('Sorry, Option C is not the right answer. Try again.', $responseData['feedback']['qn0-2']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn0-3']['correctness']);
+        $this->assertEquals('Sorry, Option D was the wrong choice. Try again.', $responseData['feedback']['qn0-3']['feedback']);
+    }
+
+    public function testGetQuestion_withFeedback_multiPart(): void
+    {
+        // Setup mocks.
+        $this->questionSetRepository
+            ->shouldReceive('getById')
+            ->andReturn($this->imasQuestionSet_dbRow_multipart);
+
+        $request = Request::create('/api/v1/question', 'POST',
+            json_decode('{
+                                  "questionSetId": 3609,
+                                  "seed": 4120
+                              }', true)
+        );
+
+        $response = $this->questionController->getQuestion($request);
+        $responseData = $response->getData(true);
+
+        $this->assertEquals(3609, $responseData['questionSetId']);
+        $this->assertEquals('multipart', $responseData['questionType']);
+        $this->assertEquals(4120, $responseData['seed']);
+        $this->assertEquals([], $responseData['errors']);
+        $this->assertNotEmpty($responseData['feedback']);
+
+        $this->assertEquals('correct', $responseData['feedback']['qn1000-0']['correctness']);
+        $this->assertEquals('Excellent choice.', $responseData['feedback']['qn1000-0']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn1000-1']['correctness']);
+        $this->assertEquals('Nope.', $responseData['feedback']['qn1000-1']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn1000-2']['correctness']);
+        $this->assertEquals('Try again.', $responseData['feedback']['qn1000-2']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn1000-3']['correctness']);
+        $this->assertEquals('Not even close.', $responseData['feedback']['qn1000-3']['feedback']);
+
+        $this->assertEquals('incorrect', $responseData['feedback']['qn1001-0']['correctness']);
+        $this->assertEquals('Nope!', $responseData['feedback']['qn1001-0']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn1001-1']['correctness']);
+        $this->assertEquals('No.', $responseData['feedback']['qn1001-1']['feedback']);
+        $this->assertEquals('correct', $responseData['feedback']['qn1001-2']['correctness']);
+        $this->assertEquals('Correct.', $responseData['feedback']['qn1001-2']['feedback']);
+
+        $this->assertEquals('correct', $responseData['feedback']['qn1002-0']['correctness']);
+        $this->assertEquals('Correct. Hawaiian pizza is always valid pizza.', $responseData['feedback']['qn1002-0']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn1002-1']['correctness']);
+        $this->assertEquals('Incorrect.', $responseData['feedback']['qn1002-1']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn1002-2']['correctness']);
+        $this->assertEquals('Try again.', $responseData['feedback']['qn1002-2']['feedback']);
+        $this->assertEquals('incorrect', $responseData['feedback']['qn1002-3']['correctness']);
+        $this->assertEquals('All days are acceptable pizza days.', $responseData['feedback']['qn1002-3']['feedback']);
     }
 
     public function testGetQuestion_withoutFeedback(): void
