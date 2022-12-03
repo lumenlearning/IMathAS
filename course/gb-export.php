e4b8ce85dc674b045e32edb255082ac1f4c82323
drlippman <drlippman@yahoo.com>
2022-12-03 11:30:59 -0800

    PHP8 fixes
    (cherry picked from commit c9db9ad9f328cb762a517b50ab67444c33151a39)

--- course/gb-export.php
+++ course/gb-export.php
@@ -380,7 +380,9 @@ function gbinstrdisp() {
 	if ($availshow==4) {
 		$availshow=1;
 		$hidepast = true;
-	}
+	} else {
+        $hidepast = false;
+    }
 	$gbt = gbtable();
 	echo '<table class="gb" id="myTable"><thead><tr>';
 	$n=0;
