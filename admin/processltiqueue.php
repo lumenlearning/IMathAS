1809bc52fbf4fbe40e5bb3b8fd3e889f6de9e3d6
Michael Ackerman <michael@lumenlearning.com>
2023-04-28 11:56:32 -0700

    Fixes for running processltiqueue.php via CLI.
    OHM-1170: Pull in MOM changes

--- admin/processltiqueue.php
+++ admin/processltiqueue.php
@@ -34,9 +34,9 @@ if (php_sapi_name() == "cli") {
     $_SERVER['HTTP_HOST'] = explode('//',$argv[1])[1];
 }
 
-require("../init_without_validate.php");
-require("../includes/rollingcurl.php");
-require_once('../includes/ltioutcomes.php');
+require(__DIR__ . "/../init_without_validate.php");
+require(__DIR__ . "/../includes/rollingcurl.php");
+require_once(__DIR__ . '/../includes/ltioutcomes.php');
 
 function debuglog($str) {
     #### Begin OHM-specific changes ############################################################
@@ -101,7 +101,7 @@ $scriptStartTime = time();
 
 //if called via AWS SNS, we need to return an OK quickly so it won't retry
 if (isset($_SERVER['HTTP_X_AMZ_SNS_MESSAGE_TYPE'])) {
-	require("../includes/AWSSNSutil.php");
+	require(__DIR__ . "/../includes/AWSSNSutil.php");
 	respondOK();
 }
 
