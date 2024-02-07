45373cef0e4d111eed30f30b0ae895f04ae447cc
Michael Ackerman <25309928+kittysnacks@users.noreply.github.com>
2024-02-06 19:20:22 -0800

    Fixed Phinx DB migration.

--- ohm/db/migrations/20180516183453_create_api_consumers_table.php
+++ ohm/db/migrations/20180516183453_create_api_consumers_table.php
@@ -14,9 +14,9 @@ class CreateApiConsumersTable extends AbstractMigration
 		$table = $this->table('ohm_api_consumers', ['id' => false,
 			'primary_key' => 'id']);
 		$table
-			->addColumn('id', 'string', ['limit' => 36, null => false])
-			->addColumn('name', 'string', ['limit' => 63, null => false])
-			->addColumn('description', 'string', ['limit' => 60, null => false])
+			->addColumn('id', 'string', ['limit' => 36, 'null' => false])
+			->addColumn('name', 'string', ['limit' => 63, 'null' => false])
+			->addColumn('description', 'string', ['limit' => 60, 'null' => false])
 			->addColumn('created_at', 'integer', ['null' => false])
 			->addColumn('updated_at', 'integer', ['null' => true])
 			->save();
