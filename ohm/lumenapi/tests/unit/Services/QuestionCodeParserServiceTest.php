2326a08276d1032b5dbafef544e7b84ffe27b43b
mphotchkiss <mphotchkiss01@gmail.com>
2025-04-30 20:40:32 -0700

    LO-1332: fix array index parens with brackets

--- ohm/lumenapi/tests/unit/Services/QuestionCodeParserServiceTest.php
+++ ohm/lumenapi/tests/unit/Services/QuestionCodeParserServiceTest.php
@@ -54,7 +54,7 @@ class QuestionCodeParserServiceTest extends TestCase
 
         $this->assertEquals('loadlibrary', $functionCalls[0]['name']);
         $this->assertEquals('"stats"', $functionCalls[0]['arguments']);
-        $this->assertEquals('ohm_getbasicfeedback', $functionCalls(count($functionCalls) - 1)['name']);
+        $this->assertEquals('ohm_getbasicfeedback', $functionCalls[count($functionCalls) - 1]['name']);
         $this->assertEquals('$stuanswers[$thisq],"Perfect!","It might be best to convert each category into percentage first before selecting the correct pie chart. Note: Percent = part/whole.",$answer', $functionCalls(count($functionCalls) - 1)['arguments']);
     }
 
