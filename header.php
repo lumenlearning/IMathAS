e983a5a3771f07d988f9e2cc794bca0ec1b58989
Annalyn Sarmiento <annalyn@lumenlearning.com>
2025-09-12 09:35:52 -0500

    OHM-1276 cleanup comments and code

--- header.php
+++ header.php
@@ -124,40 +124,37 @@ if (!isset($_SESSION['mathdisp'])) {
                 AM.newsymbol({input:v, tag:"mi", output:v, ttype:AM.TOKEN.UNARY, func:true});
             });
 
-			//format commas in numbers
-			//solution: https://groups.google.com/g/mathjax-users/c/dl8mtt-ovkM/m/OEUutu0RBAAJ?pli=1
+			//format numbers with commas
 			function addCommas(n){
-					var parts = n.toString().split(".");
-					parts[0] = parts[0].replace(/\\B(?=(\\d{3})+(?!\\d))/g, ",");
-					return parts.join(".");
-				}
+				var parts = n.toString().split(".");
+				parts[0] = parts[0].replace(/\\B(?=(\\d{3})+(?!\\d))/g, ",");
+				return parts.join(".");
+			}
 
-				function removeCommas(n) {
-					return n.replace(/,/g,"");
-				}
+			function removeCommas(n) {
+				return n.replace(/,/g,"");
+			}
 
-				function recursiveAddCommas(node) {
-					if (node.isToken) {
-						if (node.type === "mn") {
-							node.data[0].data[0] = addCommas(node.data[0].data[0]);
-						}
-					}
-					else {
-						for (var i = 0, m = node.data.length; i < m; i++) {
-							recursiveAddCommas(node.data[i]);
-						}
+			function recursiveAddCommas(node) {
+				if (node.isToken) {
+					if (node.type === "mn") {
+						node.data[0].data[0] = addCommas(node.data[0].data[0]);
 					}
 				}
+				else {
+					for (var i = 0, m = node.data.length; i < m; i++) {
+						recursiveAddCommas(node.data[i]);
+					}
+				}
+			}
 
-				MathJax.InputJax.AsciiMath.prefilterHooks.Add(function (data) {
-					data.math = data.math.replace(/\\d{1,3}(?:\\,\\d{3})+/g,removeCommas);
-				});
+			MathJax.InputJax.AsciiMath.prefilterHooks.Add(function (data) {
+				data.math = data.math.replace(/\\d{1,3}(?:\\,\\d{3})+/g,removeCommas);
+			});
 
-				MathJax.InputJax.AsciiMath.postfilterHooks.Add(function (data) {
-					recursiveAddCommas(data.math.root);
-				});
-
-			
+			MathJax.InputJax.AsciiMath.postfilterHooks.Add(function (data) {
+				recursiveAddCommas(data.math.root);
+			});	
         });
 		
 
