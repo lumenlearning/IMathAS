1c161b7c04029913efad064f9bb2127b8784d9a2
David Lippman <drlippman@yahoo.com>
2026-07-03 10:20:20 -0700

    Bug fix: -oo not recording correctly
    AMtoMQ change resulted in "- oo", messing with scoring

--- init.php
+++ init.php
@@ -22,7 +22,7 @@ if (isset($CFG['hooks']['init'])) {
 	require_once $CFG['hooks']['init'];
 }
 
-$lastvueupdate = '20260702';
+$lastvueupdate = '20260703';
 
 // setup session stuff
 if (!function_exists('disallowsSameSiteNone')) {
--- mathquill/AMtoMQ.js
+++ mathquill/AMtoMQ.js
@@ -838,7 +838,7 @@ function MQtoAM(tex,display) {
 	tex = tex.replace(/\\langle/g,' << ');
 	tex = tex.replace(/\\rangle/g,' >> ');
 	tex = tex.replace(/\\cdot/g,' * ');
-	tex = tex.replace(/\\infty/g,' oo ');
+	tex = tex.replace(/\\infty/g,' oo ').replace(/- oo/g, '-oo');
 	tex = tex.replace(/\\nthroot/g,'root');
   tex = tex.replace(/\\overline/g,'bar');
   tex = tex.replace(/\\mid/g,'|');
